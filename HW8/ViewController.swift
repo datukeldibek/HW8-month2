5a274e8e649b7cd7f09f2c3994b45357f142cd86
Datkayim <dkeldibekova08@gmail.com>
2023-02-06 14:44:47 +0600

    update

--- HW8/ViewController.swift
+++ HW8/ViewController.swift
@@ -9,6 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNumber: UITextField!
+    @IBOutlet weak var secondNumber: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+
+    @IBAction func plusButton(_ sender: Any) {
+        let firstNum = firstNumber.text
+        let secondNum = secondNumber.text
+        
+        resultLabel.text = String(Int(firstNum!)! + Int(secondNum!)!)
+    }
+    
+    @IBAction func minusButton(_ sender: Any) {
+        let firstNum = firstNumber.text
+        let secondNum = secondNumber.text
+        
+        resultLabel.text = String(Int(firstNum!)! - Int(secondNum!)!)
+    }
+    
+    @IBAction func multiplyButton(_ sender: Any) {
+        let firstNum = firstNumber.text
+        let secondNum = secondNumber.text
+        
+        resultLabel.text = String(Int(firstNum!)! * Int(secondNum!)!)
+    }
+    
+    @IBAction func divideButton(_ sender: Any) {
+        let firstNum = firstNumber.text
+        let secondNum = secondNumber.text
+        
+        resultLabel.text = String(Double(firstNum!)! / Double(secondNum!)!)
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
